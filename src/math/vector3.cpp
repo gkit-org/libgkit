0733aec7714ca07fc073f90ee9ab50c9830905b1
YuanSang <3152937201@qq.com>
2026-04-24 20:57:25 +0800

    fix

--- src/math/vector3.cpp
+++ src/math/vector3.cpp
@@ -3,3 +3,4 @@
 gkit::math::Vector3::Vector3(float v) noexcept : x(v), y(v), z(v) { }
 gkit::math::Vector3::Vector3(float x, float y, float z) noexcept : x(x), y(y), z(z) { }
 gkit::math::Vector3::Vector3(const gkit::math::Vector2& v, float z) noexcept : x(v.x), y(v.y), z(z) { }
+gkit::math::Vector3::Vector3(const gkit::math::Vector3&& other) noexcept : x(other.x), y(other.y), z(other.z) { }
